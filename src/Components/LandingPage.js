569c973d9f8f9952432712a800fd992112bfbaa2
Filip Koniuszewski <filip7446@gmail.com>
2022-04-16 19:42:42 +0200

    implemented intro section

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -1,9 +1,26 @@
 ﻿import React from 'react';
+import intro from "../Images/illustration-intro.png";
+import "./Intro.css";
+
 
 function LandingPage(props) {
     return (
         <main className="Main">
-            Page
+            <section className="intro">
+                <img src={intro} alt="" />
+                <h1>
+                    All your files in one secure
+                    location, accessible anywhere.
+                </h1>
+                <p>
+                    Fylo stores all your most important files in
+                    one secure location. Access them wherever you need, 
+                    share and collaborate with friends family, and co-workers.
+                </p>
+                <div className="get-started">
+                    Get Started
+                </div>
+            </section>
         </main>
     );
 }
