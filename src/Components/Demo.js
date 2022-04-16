8b167cc8cfdc6a7ab5d3a9452fa745722b3f3573
Filip Koniuszewski <filip7446@gmail.com>
2022-04-16 21:22:18 +0200

    implemented demo section

--- src/Components/Demo.js
+++ src/Components/Demo.js
@@ -1,10 +1,33 @@
 ﻿import React from 'react'
 import "./Demo.css"
+import demoImage from "../Images/illustration-stay-productive.png"
+import goToDemo from "../Images/icon-arrow.svg"
 
 function Demo(props) {
     return (
         <section className="demo"> 
-            
+            <div className="demo-content">
+                <img src={demoImage} alt="" />
+                <div className="demo-text-content">
+                    <h1 className="demo-header">
+                        Stay productive, wherever you are
+                    </h1>
+                    <p className="demo-paragraph">
+                        Never let location be an issue when accessing
+                        your files. Fylo has you covered for all of your
+                        file storage needs.
+                    </p>
+                    <p className="demo-paragraph">
+                        Securely share files and 
+                        folders with friends, family and colleagues for 
+                        live collaboration. No email attachments required.
+                    </p>
+                    <div className="go-to-demo">
+                        See how Fylo works
+                        <img src={goToDemo} alt="" />
+                    </div>
+                </div>
+            </div>
         </section>
     );
 }
